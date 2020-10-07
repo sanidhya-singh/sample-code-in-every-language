16c33c5677cf9eb3db03f7078e5d47d4336c1477
Scott Little <50145141+CodeLongAndProsper90@users.noreply.github.com>
2020-10-07 14:18:07 -0500

    Create factorial.d

---
+++ d/factorial.d
@@ -0,0 +1,19 @@
+
+import std.stdio: writeln;
+import std.getopt;
+int fac(int a)
+{
+  if (a <= 1)
+    {
+      return 1;
+    }
+  return a*fac(a-1);
+}
+int main(string[] args)
+{
+  int depth;
+  auto params = getopt(args, "depth", &depth);
+  int result = fac(depth);
+  writeln(result);
+  return 0;
+}
